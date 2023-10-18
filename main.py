624f45215000a366e047d90913f05cf119e5cb9f
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-10-18 17:53:35 -0400

    Create CarManager class

--- main.py
+++ main.py
@@ -15,11 +15,20 @@ screen.onkey(turtle_player.move, "Up")
 
 # Create Car Manager
 car_manager = CarManager()
-car_manager.create_cars()
+
 
 game_is_on = True
+index = 0
 while game_is_on:
+    if index == 5:
+        index = 0
+        car_manager.create_car()
+
+    car_manager.move_cars()
+    index += 1
     time.sleep(0.10)
     screen.update()
+
+
 
 screen.exitonclick()
